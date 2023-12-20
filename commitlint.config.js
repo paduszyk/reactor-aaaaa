0e4a0052542f325bac3f5bf6087be4df29365ecd
Kamil Paduszyński <92403542+paduszyk@users.noreply.github.com>
2023-12-20 10:50:51 +0100

    chore(commitlint): apply the conventional commits rules

---
+++ commitlint.config.js
@@ -0,0 +1,10 @@
+/**
+ * Commitlint
+ * https://commitlint.js.org/#/reference-configuration
+ */
+
+const Configuration = {
+  extends: ["@commitlint/config-conventional"],
+};
+
+module.exports = Configuration;
