0e251e14f975d7db929e56ea7be4cd5c9d0e1726
Kamil Paduszyński <92403542+paduszyk@users.noreply.github.com>
2023-12-20 10:50:57 +0100

    chore(commitlint): ignore messages for merge commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,6 +5,9 @@
 
 const Configuration = {
   extends: ["@commitlint/config-conventional"],
+  ignores: [
+    (message) => /[A-Z].*\s\(#\d+\)\n?/.test(message),
+  ],
 };
 
 module.exports = Configuration;
